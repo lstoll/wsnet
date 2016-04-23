17dcfb9603b5b8b062208a07971986c7ead2e04c
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-04-22 20:50:03 -0400

    Basic end to end test

---
+++ wsnet_test.go
@@ -0,0 +1,55 @@
+package wsnet
+
+import (
+	"bufio"
+	"fmt"
+	"testing"
+	"time"
+)
+
+const listenPort = "19546"
+
+func TestEndToEnd(t *testing.T) {
+	// Start a server
+	server, err := Listen("127.0.0.1:" + listenPort)
+	defer server.Close()
+	if server == nil {
+		t.Fatal("couldn't start listening: ", err)
+	}
+	go func() {
+		for {
+			client, err := server.Accept()
+			if client == nil {
+				t.Fatal("couldn't accept: ", err)
+			}
+			b := bufio.NewReader(client)
+			for {
+				line, err := b.ReadBytes('\n')
+				if err != nil { // EOF, or worse
+					break
+				}
+				client.Write(line)
+			}
+		}
+	}()
+
+	for i := 0; i < 5; i++ {
+		conn, err := Dial("ws://127.0.0.1:"+listenPort, 2*time.Second)
+		if err != nil {
+			t.Fatal(err)
+		}
+		for x := 0; x < 10; x++ {
+			fmt.Fprintf(conn, "PING\n")
+			resp, err := bufio.NewReader(conn).ReadString('\n')
+			if err != nil {
+				t.Fatal(err)
+			}
+			if resp != "PING\n" {
+				t.Fatalf("Expected %q, got %q", "PING\n", resp)
+			}
+		}
+		conn.Close()
+	}
+
+	// Connect to if a few times and have a chat
+}
