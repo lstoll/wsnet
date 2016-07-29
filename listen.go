e10e987be7426f6587a9aa64325ca4e880540ddd
Lincoln Stoll <lincoln.stoll@gmail.com>
2016-07-28 20:44:39 -0400

    Option to just get a handler, rather than listen
    This can be useful for embedding on existing servers

--- listen.go
+++ listen.go
@@ -14,10 +14,17 @@ type wsServer struct {
 	pingInterval time.Duration
 }
 
+type wsaddr struct{}
+
+// Listen will start a HTTP server on the given listener, and return the
+// net.Listener for the websocket handler on "/"
 func Listen(laddr string) (net.Listener, error) {
 	return ListenWithKeepalive(laddr, 0)
 }
 
+// ListenWithKeepalive will start a HTTP server on the given listener, and
+// return the net.Listener for the websocket handler on "/". It will send
+// traffic down the connection every pingInterval to keep it alive.
 func ListenWithKeepalive(laddr string, pingInterval time.Duration) (net.Listener, error) {
 	listener, err := net.Listen("tcp", laddr)
 	if err != nil {
@@ -33,17 +40,40 @@ func ListenWithKeepalive(laddr string, pingInterval time.Duration) (net.Listener
 	return wss, nil
 }
 
+// Handler returns a net.Listener that will be a listener for traffic received
+// over the websocket http.Handler
+func Handler() (net.Listener, http.Handler) {
+	return HandlerWithKeepalive(0)
+}
+
+// HandlerWithKeepalive returns a net.Listener that will be a listener for
+// traffic received over the websocket http.Handler. It will send traffic down
+// the connection every pingInterval to keep it alive.
+func HandlerWithKeepalive(pingInterval time.Duration) (net.Listener, http.Handler) {
+	wss := &wsServer{
+		conns:        make(chan *wsConn),
+		pingInterval: pingInterval,
+	}
+	return wss, websocket.Handler(wss.wsHandler)
+}
+
 func (w *wsServer) Accept() (net.Conn, error) {
 	return <-w.conns, nil
 }
 
 func (w *wsServer) Close() error {
-	return w.listener.Close()
+	if w.listener != nil {
+		return w.listener.Close()
+	}
+	return nil
 }
 
 func (w *wsServer) Addr() net.Addr {
 	// This is still legit enough? Maybe?
-	return w.listener.Addr()
+	if w.listener != nil {
+		return w.listener.Addr()
+	}
+	return &wsaddr{}
 }
 
 func (w *wsServer) wsHandler(ws *websocket.Conn) {
@@ -66,4 +96,12 @@ func (w *wsServer) wsHandler(ws *websocket.Conn) {
 	w.conns <- wsconn
 	<-closed
 	stopHb <- struct{}{}
+}
+
+func (w *wsaddr) Network() string {
+	return "wsnet"
+}
+
+func (w *wsaddr) String() string {
+	return "wsnet-handler"
 }
--- wsnet_test.go
+++ wsnet_test.go
@@ -3,52 +3,92 @@ package wsnet
 import (
 	"bufio"
 	"fmt"
+	"net"
+	"net/http"
 	"testing"
 	"time"
 )
 
-const listenPort = "19546"
-
 func TestEndToEnd(t *testing.T) {
 	// Start a server
-	server, err := ListenWithKeepalive("127.0.0.1:"+listenPort, 1*time.Millisecond)
-	defer server.Close()
-	if server == nil {
-		t.Fatal("couldn't start listening: ", err)
-	}
-	go func() {
-		for {
-			client, err := server.Accept()
-			if client == nil {
-				t.Fatal("couldn't accept: ", err)
-			}
-			b := bufio.NewReader(client)
-			for {
-				line, err := b.ReadBytes('\n')
+	for rn, tc := range [...]struct {
+		listener func(addr string) (net.Listener, error)
+		dialer   func(addr string) (net.Conn, error)
+	}{
+		{
+			// Straight up listener/dialer
+			listener: func(addr string) (net.Listener, error) { return ListenWithKeepalive(addr, 1*time.Millisecond) },
+			dialer:   func(addr string) (net.Conn, error) { return Dial("ws://"+addr, 2*time.Second) },
+		},
+		{
+			// Custom handler stuff
+			listener: func(addr string) (net.Listener, error) {
+				hl, err := net.Listen("tcp", addr)
 				if err != nil {
-					break
+					return nil, err
 				}
-				client.Write(line)
-			}
-		}
-	}()
-
-	for i := 0; i < 5; i++ {
-		conn, err := Dial("ws://127.0.0.1:"+listenPort, 2*time.Second)
+				lis, h := HandlerWithKeepalive(2 * time.Second)
+				serveMux := http.NewServeMux()
+				serveMux.Handle("/wsnet", h)
+				go func() {
+					err = http.Serve(hl, serveMux)
+					if err != nil {
+						panic(err)
+					}
+				}()
+				return lis, nil
+			},
+			dialer: func(addr string) (net.Conn, error) { return Dial("ws://"+addr+"/wsnet", 2*time.Second) },
+		},
+	} {
+		// get someone to allocate us a port
+		lis, err := net.Listen("tcp", "127.0.0.1:0")
 		if err != nil {
 			t.Fatal(err)
 		}
-		for x := 0; x < 10; x++ {
-			fmt.Fprintf(conn, "PING\n")
-			resp, err := bufio.NewReader(conn).ReadString('\n')
+		addr := lis.Addr().String()
+		lis.Close()
+
+		t.Logf("Starting case %d", rn)
+		server, err := tc.listener(addr)
+		defer server.Close()
+		if server == nil {
+			t.Fatal("couldn't start listening: ", err)
+		}
+		go func() {
+			for {
+				client, err := server.Accept()
+				if client == nil {
+					t.Fatal("couldn't accept: ", err)
+				}
+				b := bufio.NewReader(client)
+				for {
+					line, err := b.ReadBytes('\n')
+					if err != nil {
+						break
+					}
+					client.Write(line)
+				}
+			}
+		}()
+
+		for i := 0; i < 5; i++ {
+			conn, err := tc.dialer(addr)
 			if err != nil {
 				t.Fatal(err)
 			}
-			if resp != "PING\n" {
-				t.Fatalf("Expected %q, got %q", "PING\n", resp)
+			for x := 0; x < 10; x++ {
+				fmt.Fprintf(conn, "PING\n")
+				resp, err := bufio.NewReader(conn).ReadString('\n')
+				if err != nil {
+					t.Fatal(err)
+				}
+				if resp != "PING\n" {
+					t.Fatalf("Expected %q, got %q", "PING\n", resp)
+				}
+				time.Sleep(5 * time.Millisecond)
 			}
-			time.Sleep(5 * time.Millisecond)
+			conn.Close()
 		}
-		conn.Close()
 	}
 }
